283f90f0f69a41f67d90dbf4959c07769865144a
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-03 22:38:41 +0900

    First Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+//변수를 활용하는 방법
+function Food({ fav }){
+  //console.log(props.fav);
+  console.log(fav);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>I like {fav}</h3>
     </div>
   );
 }
+
+function App() {
+  return(
+    <div>    
+      <h1>하이이이</h1>
+      <Food fav="kimchi"/>
+      <Food fav="Potato"/>
+
+    </div>
+    );
+}
+  
 
 export default App;
