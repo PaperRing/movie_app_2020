d556c004b9361e3e7fddef3cd754391b4dadfa70
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-08 14:27:53 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//변수를 활용하는 방법
-function Food({ name, pic, rating }){
-  return (
+function Food ({name, img, rating}){
+  return(
     <div>
       <h3>I like {name}</h3>
-      <img src={pic} alt={name}/>
-      <div>rating : {rating} / 5.0</div>
+      <img src={img} alt={name}/>
+      <div>rating : {rating}/5.0</div>
     </div>
-  );  
+  );
 }
 
-//제대로된 변수가 들어갔는지 확인하는 기능
 Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
+  name : PropTypes.string.isRequired
 };
 
 const foodILike = [
   {
     id : 1,
     name : "potato",
-    image :
+    image :     
+    "https://images.eatsmarter.de/sites/default/files/styles/576x432/public/images/kartoffeln-vitamin-c-bombe-341x256.jpg",
+    rating : 4
+  },
+  {
+    id : 2,
+    name : "kimchi",
+    image :     
     "https://images.eatsmarter.de/sites/default/files/styles/576x432/public/images/kartoffeln-vitamin-c-bombe-341x256.jpg",
     rating : 5
   }
 ];
 
-
-function App() {
+function App(){
   return(
     <div>
-      {/*console.log(foodILike.map(renderFood))*/}
       {foodILike.map(dish =>(
-        <Food name={dish.name} pic={dish.image} rating={dish.rating} key={dish.id}/>
+        <Food key={dish.id} name={dish.name} img={dish.image} rating={dish.rating}/>
       ))}
+      {console.log(foodILike.map(dish=>(
+        <Food key={dish.id} name={dish.name} img={dish.image} rating={dish.rating}/>
+      )))}
     </div>
-    );
+  );
 }
-  
 
 export default App;
