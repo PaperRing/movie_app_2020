22db3f24379419403870bd5097e0972c807546eb
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-08 23:47:06 +0900

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//class component
-//return 대신 render 사용
 class App extends React.Component{
-  //변화하는 데이터
-  //state is object
   state = {
-    count: 0
+    isLoading : true,
+    movies: []
   };
-
-  add = () => {
-    //render을 부르려면 setState를 써야함
-    //state에 의존하지 않으려면 current 사용하는 것이 best
-    this.setState(current => ({ count : current.count + 1}))
-  };
-
-  minus = () => {
-    this.setState(current => ({ count : current.count - 1}))
-  };
-
+  //render 다음으로 호출 
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 3000);
+  }
   render(){
-  return (
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>add</button>
-      <button onClick={this.minus}>minus</button>
-    </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ?  "Loading..." : "We are readey!"}</div>
   }
 }
 
