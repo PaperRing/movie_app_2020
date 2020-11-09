d1d263df0e2a9bf613cb83bf4990a4cf5a9a662d
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-09 22:23:26 +0900

    Rendering the Movie

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
   state = {
     isLoading : true,
     movies: []
   };
+  //비동기 호출, axios 가 호출되는데 시간이 좀 걸리니까 기다리라고 말해주는 async & await
+  getMovies = async () =>{
+    const {data: {data: { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
   //render 다음으로 호출 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 3000);
-  }
+    this.getMovies();
+  };
   render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ?  "Loading..." : "We are readey!"}</div>
+    const { isLoading, movies } = this.state;
+    return (
+    <section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div> 
+        ) : (
+          <div class="movies">
+            {movies.map(movie =>(
+              <Movie 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                key={movie.id} 
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+          
+        )}
+    </section>
+    );
   }
-}
+};
 
 export default App;
