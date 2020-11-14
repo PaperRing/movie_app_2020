10f77cfa41cbed823bb6422ebef7c9dbb43143d1
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-14 14:08:06 +0900

    Genres and Summary

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
-  state = {
-    isLoading : true,
-    movies: []
-  };
-  //비동기 호출, axios 가 호출되는데 시간이 좀 걸리니까 기다리라고 말해주는 async & await
-  getMovies = async () =>{
-    const {data: {data: { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });
-  };
-  //render 다음으로 호출 
-  componentDidMount(){
-    this.getMovies();
-  };
-  render(){
-    const { isLoading, movies } = this.state;
-    return (
-    <section class="container">
-      {isLoading ? (
-        <div class="loader">
-          <span class="loader__text">Loading...</span>
-        </div> 
-        ) : (
-          <div class="movies">
-            {movies.map(movie =>(
-              <Movie 
-                id={movie.id} 
-                year={movie.year} 
-                title={movie.title} 
-                summary={movie.summary} 
-                key={movie.id} 
-                poster={movie.medium_cover_image}
-              />
-            ))}
-          </div>
-          
-        )}
-    </section>
-    );
-  }
-};
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    //비동기 호출, axios 가 호출되는데 시간이 좀 걸리니까 기다리라고 말해주는 async & await
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+            //fetch 대신 axios 사용
+        } = await axios.get(
+            "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+        );
+        this.setState({ movies, isLoading: false });
+    };
+    //render 다음으로 호출
+    componentDidMount() {
+        this.getMovies();
+    }
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map((movie) => (
+                            <Movie
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                key={movie.id}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
+        );
+    }
+}
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css';
-
-function Moive( {id, title, year, summary, poster}){
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+function Movie({ year, title, summary, poster, genres }) {
     return (
-      <div class="movies__movie">
-        <img src={poster} alt={title} title={title}/>
-        <div class="movies__data">
-          <h3 class="movie__title">{title}</h3>
-          <h5 class="movice__year">{year}</h5>
-          <p class="movie__summary">{summary}</p> 
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 140)}...</p>
+            </div>
         </div>
-      </div>
     );
 }
-
-Moive.propTypes = {
+Movie.propTypes = {
     id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
     year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
-
-export default Moive;
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default Movie;
