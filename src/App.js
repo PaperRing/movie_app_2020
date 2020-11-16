8e5e22a608fe6c2f569098ac42d8c4fea1690f09
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-16 19:44:00 +0900

    Complete First Version of Moive App

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Detail from "./components/Detail";
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: [],
-    };
-    //비동기 호출, axios 가 호출되는데 시간이 좀 걸리니까 기다리라고 말해주는 async & await
-    getMovies = async () => {
-        const {
-            data: {
-                data: { movies },
-            },
-            //fetch 대신 axios 사용
-        } = await axios.get(
-            "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-        );
-        this.setState({ movies, isLoading: false });
-    };
-    //render 다음으로 호출
-    componentDidMount() {
-        this.getMovies();
-    }
-    render() {
-        const { isLoading, movies } = this.state;
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader__text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map((movie) => (
-                            <Movie
-                                id={movie.id}
-                                year={movie.year}
-                                title={movie.title}
-                                summary={movie.summary}
-                                key={movie.id}
-                                poster={movie.medium_cover_image}
-                                genres={movie.genres}
-                            />
-                        ))}
-                    </div>
-                )}
-            </section>
-        );
-    }
+function App() {
+    return (
+        <HashRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/movie/:id" component={Detail}></Route>
+        </HashRouter>
+    );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,55 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    //비동기 호출, axios 가 호출되는데 시간이 좀 걸리니까 기다리라고 말해주는 async & await
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+            //fetch 대신 axios 사용
+        } = await axios.get(
+            "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+        );
+        this.setState({ movies, isLoading: false });
+    };
+    //render 다음으로 호출
+    componentDidMount() {
+        this.getMovies();
+    }
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map((movie) => (
+                            <Movie
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                key={movie.id}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
+        );
+    }
+}
+
+export default Home;
